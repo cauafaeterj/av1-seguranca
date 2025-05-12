9b3575b91ee749500a28abf335dcbbfe1e49a825
cauafaeterj <caua.23204708360029@faeterj-rio.edu.br>
2025-05-12 08:44:38 -0300

    Update __init__.py

--- 1745964050974+auth-api (1)/auth-api-main/app/routes/__init__.py
+++ 1745964050974+auth-api (1)/auth-api-main/app/routes/__init__.py
@@ -2,5 +2,5 @@ from flask import Blueprint
 from app.routes.routes import user_bp
 
 def register_routes(app):
-    """Registra todos os Blueprints de rotas na aplicação Flask."""
+    
     app.register_blueprint(user_bp, url_prefix='/user')
