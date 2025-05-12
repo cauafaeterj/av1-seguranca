2ecbfb79d679bd781318ec59b78a5062fddba025
cauafaeterj <caua.23204708360029@faeterj-rio.edu.br>
2025-05-12 08:47:42 -0300

    Update config.py

--- 1745964050974+auth-api (1)/auth-api-main/app/config.py
+++ 1745964050974+auth-api (1)/auth-api-main/app/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    # Configuração do banco de dados SQLite
+     
     DATABASE_URL = os.getenv('DATABASE_URL')
     if not DATABASE_URL:
         raise ValueError("DATABASE_URL não foi encontrada no ambiente. Verifique o arquivo .env.")
@@ -9,5 +9,5 @@ class Config:
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
-    # Configuração da SECRET_KEY para Flask-Session
+     
     SECRET_KEY = os.getenv('SECRET_KEY')
