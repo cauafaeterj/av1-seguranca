bede046566ad537b1fb8b9ada2590f79a5becf6f
cauafaeterj <caua.23204708360029@faeterj-rio.edu.br>
2025-05-12 08:41:11 -0300

    Update run.py

--- 1745964050974+auth-api (1)/auth-api-main/run.py
+++ 1745964050974+auth-api (1)/auth-api-main/run.py
@@ -3,17 +3,16 @@ from app import create_app
 from dotenv import load_dotenv
 import os
 
-# Carrega variáveis de ambiente do arquivo .env
+
 load_dotenv()
 
-# Cria a instância da aplicação Flask
+ 
 app = create_app()
 
 if __name__ == "__main__":
-    # Obtém configurações do .env, com valores padrão consistentes
     host = os.getenv('APP_HOST', '127.0.0.1')
     port = int(os.getenv('APP_PORT', '8081'))  
     debug = os.getenv('FLASK_DEBUG', '0') == '1'
     
-    # Inicia o servidor Flask
+     
     app.run(host=host, port=port, debug=debug)
